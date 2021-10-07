8e6aedcad58a83a4493e02910c9311e3d7ffbfe7
Matthias Fasching <matthias.fasching@a52.eu>
2021-10-07 11:24:00 +0000

    Fix Warnings that are displayed during buffalo new

--- internal/genny/plugins/install/install_test.go
+++ internal/genny/plugins/install/install_test.go
@@ -44,7 +44,7 @@ func Test_New(t *testing.T) {
 
 	res := run.Results()
 
-	ecmds := []string{"go get -tags sqlite github.com/gobuffalo/buffalo-pop/v2"}
+	ecmds := []string{"go install -tags sqlite github.com/gobuffalo/buffalo-pop/v2@latest"}
 	r.Len(res.Commands, len(ecmds))
 	for i, c := range res.Commands {
 		r.Equal(ecmds[i], strings.Join(c.Args, " "))
