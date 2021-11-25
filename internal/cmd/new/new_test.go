219aa9328dc4fc23b44968170574ee0a12d4ef1a
Antonio Pagano <apagano@wawand.co>
2021-11-25 16:51:31 -0500

    adding test for dockerfile

---
+++ internal/cmd/new/new_test.go
@@ -0,0 +1,69 @@
+package new
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	dir := os.TempDir()
+	err := os.Chdir(dir)
+	if err != nil {
+		t.Fatal("error changing to tmp dir")
+	}
+
+	err = os.RemoveAll(filepath.Join(dir, "app"))
+	if err != nil {
+		t.Fatal("error removing new dir")
+	}
+
+	tcases := []struct {
+		name  string
+		args  []string
+		check func() error
+	}{
+		{
+			name: "skip docker",
+			args: []string{"app", "--api", "--skip-docker", "-f"},
+			check: func() error {
+				_, err := os.Stat(filepath.Join("app", "Dockerfile"))
+				if err == nil {
+					return fmt.Errorf("Dockerfile should not be generated")
+				}
+
+				return nil
+			},
+		},
+
+		{
+			name: "docker there",
+			args: []string{"app", "--api", "-f"},
+			check: func() error {
+				_, err := os.Stat(filepath.Join("app", "Dockerfile"))
+				if err != nil {
+					return fmt.Errorf("Dockerfile should be there")
+				}
+
+				return nil
+			},
+		},
+	}
+
+	for _, v := range tcases {
+		t.Run(v.name, func(t *testing.T) {
+			Cmd.SetArgs(v.args)
+			err = Cmd.Execute()
+			if err != nil {
+				t.Fatal("error running new")
+			}
+
+			err = v.check()
+			if err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+
+}
---
+++ internal/cmd/new/options.go
@@ -0,0 +1,168 @@
+package new
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"strings"
+
+	pop "github.com/gobuffalo/buffalo-pop/v2/genny/newapp"
+	"github.com/gobuffalo/cli/internal/genny/ci"
+	"github.com/gobuffalo/cli/internal/genny/docker"
+	"github.com/gobuffalo/cli/internal/genny/newapp/core"
+	"github.com/gobuffalo/cli/internal/genny/refresh"
+	"github.com/gobuffalo/cli/internal/genny/vcs"
+	fname "github.com/gobuffalo/flect/name"
+	"github.com/gobuffalo/meta"
+	"github.com/spf13/viper"
+)
+
+var configError error
+
+type options struct {
+	Options *core.Options
+	Module  string
+	Force   bool
+	Verbose bool
+	DryRun  bool
+}
+
+func parseNewOptions(args []string) (options, error) {
+	nopts := options{
+		Force:   viper.GetBool("force"),
+		Verbose: viper.GetBool("verbose"),
+		DryRun:  viper.GetBool("dry-run"),
+		Module:  viper.GetString("module"),
+	}
+
+	if len(args) == 0 {
+		return nopts, fmt.Errorf("you must enter a name for your new application")
+	}
+	if configError != nil {
+		return nopts, configError
+	}
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		return nopts, err
+	}
+	app := meta.New(pwd)
+	app.WithGrifts = true
+	app.Name = fname.New(args[0])
+	app.Bin = filepath.Join("bin", app.Name.String())
+
+	if app.Name.String() == "." {
+		app.Name = fname.New(filepath.Base(app.Root))
+	} else {
+		app.Root = filepath.Join(app.Root, app.Name.File().String())
+	}
+
+	if len(nopts.Module) == 0 {
+		aa := meta.New(app.Root)
+		app.PackageRoot(aa.PackagePkg)
+	} else {
+		app.PackageRoot(nopts.Module)
+	}
+
+	app.AsAPI = viper.GetBool("api")
+	app.VCS = viper.GetString("vcs")
+
+	app.WithPop = !viper.GetBool("skip-pop")
+	app.WithWebpack = !viper.GetBool("skip-webpack")
+	app.WithYarn = !viper.GetBool("skip-yarn")
+	app.WithNodeJs = app.WithWebpack
+	app.AsWeb = !app.AsAPI
+
+	if app.AsAPI {
+		app.WithWebpack = false
+		app.WithYarn = false
+		app.WithNodeJs = false
+	}
+
+	opts := &core.Options{}
+
+	if x := viper.GetBool("skip-docker"); !x {
+		opts.Docker = &docker.Options{}
+	}
+
+	app.WithDocker = !viper.GetBool("skip-docker")
+
+	if x := viper.GetString("ci-provider"); len(x) > 0 && x != "none" {
+		opts.CI = &ci.Options{
+			Provider: x,
+			DBType:   viper.GetString("db-type"),
+		}
+	}
+
+	if len(app.VCS) > 0 && app.VCS != "none" {
+		opts.VCS = &vcs.Options{
+			Provider: app.VCS,
+		}
+	}
+
+	if app.WithPop {
+		d := viper.GetString("db-type")
+		if d == "sqlite3" {
+			app.WithSQLite = true
+		}
+
+		opts.Pop = &pop.Options{
+			Prefix:  app.Name.File().String(),
+			Dialect: d,
+		}
+	}
+
+	opts.Refresh = &refresh.Options{}
+
+	opts.App = app
+	nopts.Options = opts
+
+	return nopts, nil
+}
+
+func initConfig(skipConfig *bool, cfgFile *string) func() {
+	return func() {
+		if *skipConfig {
+			return
+		}
+
+		var err error
+		if *cfgFile != "" { // enable ability to specify config file via flag
+			viper.SetConfigFile(*cfgFile)
+			// Will error only if the --config flag is used
+			if err = viper.ReadInConfig(); err != nil {
+				configError = err
+			}
+		} else {
+			viper.SetConfigName(".buffalo") // name of config file (without extension)
+			viper.AddConfigPath("$HOME")    // adding home directory as first search path
+			viper.AutomaticEnv()            // read in environment variables that match
+			viper.ReadInConfig()
+		}
+
+	}
+}
+
+func currentUser() (string, error) {
+	_, err := exec.LookPath("git")
+	if err == nil {
+		b, err := exec.Command("git", "config", "github.user").Output()
+		if err == nil {
+			return string(b), nil
+		}
+	}
+
+	u, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+
+	username := u.Username
+	if t := strings.Split(username, `\`); len(t) > 0 {
+		username = t[len(t)-1]
+	}
+
+	return username, nil
+}
