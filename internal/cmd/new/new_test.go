e623473e819dbb3e728dc32128879d365b8fa2b8
Antonio Pagano <apagano@wawand.co>
2021-11-25 20:23:07 -0500

    moving to require

--- internal/cmd/new/new_test.go
+++ internal/cmd/new/new_test.go
@@ -1,68 +1,49 @@
 package new
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestNew(t *testing.T) {
+	r := require.New(t)
 	dir := os.TempDir()
-	err := os.Chdir(dir)
-	if err != nil {
-		t.Fatal("error changing to tmp dir")
-	}
 
-	err = os.RemoveAll(filepath.Join(dir, "app"))
-	if err != nil {
-		t.Fatal("error removing new dir")
-	}
+	r.NoError(os.Chdir(dir))
+	r.NoError(os.RemoveAll(filepath.Join(dir, "app")))
 
 	tcases := []struct {
 		name  string
 		args  []string
-		check func() error
+		check func(*require.Assertions)
 	}{
 		{
 			name: "skip docker",
 			args: []string{"app", "--api", "--skip-docker", "-f", "--vcs", "none"},
-			check: func() error {
-				_, err := os.Stat(filepath.Join("app", "Dockerfile"))
-				if err == nil {
-					return fmt.Errorf("Dockerfile should not be generated")
-				}
-
-				return nil
+			check: func(r *require.Assertions) {
+				r.NoFileExists(filepath.Join("app", "Dockerfile"))
 			},
 		},
 
 		{
 			name: "docker there",
 			args: []string{"app", "--api", "-f", "--vcs", "none"},
-			check: func() error {
-				_, err := os.Stat(filepath.Join("app", "Dockerfile"))
-				if err != nil {
-					return fmt.Errorf("Dockerfile should be there")
-				}
-
-				return nil
+			check: func(r *require.Assertions) {
+				r.FileExists(filepath.Join("app", "Dockerfile"))
 			},
 		},
 	}
 
 	for _, v := range tcases {
 		t.Run(v.name, func(t *testing.T) {
+			r := require.New(t)
 			Cmd.SetArgs(v.args)
-			err = Cmd.Execute()
-			if err != nil {
-				t.Fatal("error running new")
-			}
 
-			err = v.check()
-			if err != nil {
-				t.Fatal(err)
-			}
+			r.NoError(Cmd.Execute())
+			v.check(r)
 		})
 	}
 
