0fed4ecccd301c0fd76c3a6c04e35385a316a13f
Matthias Fasching <Matthias.Fasching@zuehlke.com>
2021-11-27 23:37:42 +0000

    Move fixers to different branch

--- internal/testhelpers/testhelpers.go
+++ internal/testhelpers/testhelpers.go
@@ -10,15 +10,6 @@ import (
 	"golang.org/x/mod/modfile"
 )
 
-// InstallBuffaloCMD installs a specific version of buffalo, it should be run before
-// calling RunBuffaloCMD to ensure this is the version of buffalo that is being tested.
-func InstallBuffaloCMD(t *testing.T, version string) error {
-	t.Helper()
-
-	ex := exec.Command("go", "install", "-tags", "sqlite", fmt.Sprintf("github.com/gobuffalo/cli/cmd/buffalo@%s", version))
-	return ex.Run()
-}
-
 // EnsureBuffaloCMD installs current version of buffalo, it should be run before
 // calling RunBuffaloCMD to ensure this is the version of buffalo that is being tested.
 func EnsureBuffaloCMD(t *testing.T) error {
@@ -37,7 +28,7 @@ func EnsureBuffaloCMD(t *testing.T) error {
 	return ex.Run()
 }
 
-// incCLISource ensures that the current directory is the CLI source folder by
+// inCLISource ensures that the current directory is the CLI source folder by
 // checking its parent go.mod file says its github.com/gobuffalo/cli module.
 func inCLISource() (bool, error) {
 	wd, err := os.Getwd()
