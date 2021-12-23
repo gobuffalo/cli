3914d8a75c5cc9610ed892c44ea7cc3de702c8dd
Matthias Fasching <Matthias.Fasching@zuehlke.com>
2021-12-23 13:17:16 +0000

    Update deprecations

--- internal/genny/fix/deprecations.go
+++ internal/genny/fix/deprecations.go
@@ -3,8 +3,12 @@ package fix
 import (
 	"bytes"
 	"fmt"
+	"go/format"
+	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gobuffalo/genny/v2"
 )
@@ -13,58 +17,88 @@ import (
 func DeprecationsCheck(opts *Options) genny.RunFn {
 	return func(r *genny.Runner) error {
 		fmt.Println("~~~ Checking for deprecations ~~~")
-		b, err := os.ReadFile("main.go")
+		f, err := r.FindFile("main.go")
 		if err != nil {
 			return err
 		}
 
-		if bytes.Contains(b, []byte("app.Start")) {
+		if strings.Contains(f.String(), "app.Start") {
 			opts.warnings = append(opts.warnings, "app.Start has been removed in v0.11.0. Use app.Serve Instead. [main.go]")
 		}
 
-		err = filepath.Walk(filepath.Join(opts.App.Root, "actions"), func(path string, info os.FileInfo, _ error) error {
-			if info.IsDir() {
-				return nil
-			}
+		err = walkDisk(r.Disk, filepath.Join(opts.App.Root, "actions"), actionsWalkFun(r.Disk, opts))
+		// TODO: add other folders to check
+		return err
+	}
+}
 
-			if filepath.Ext(path) != ".go" {
-				return nil
-			}
+func actionsWalkFun(r *genny.Disk, opts *Options) func(path string, info os.FileInfo, err error) error {
+	return func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
 
-			b, err := os.ReadFile(path)
+		if filepath.Ext(path) != ".go" {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		f, err := r.Find(path)
+		if err != nil {
+			return err
+		}
+		b, err := io.ReadAll(f)
+		if err != nil {
+			return err
+		}
+
+		if bytes.Contains(b, []byte("AssetsBox")) {
+			b = bytes.Replace(b, []byte("AssetsBox:"), []byte("AssetsFS:"), -1)
+		}
+
+		if bytes.Contains(b, []byte("TemplatesBox")) {
+			b = bytes.Replace(b, []byte("TemplatesBox:"), []byte("TemplatesFS:"), -1)
+		}
+
+		b, err = format.Source(b)
+		if err != nil {
+			return err
+		}
+
+		_, err = f.Write(b)
+		return err
+	}
+}
+
+func walkDisk(disk *genny.Disk, root string, walkFun filepath.WalkFunc) error {
+	for _, f := range disk.Files() {
+		rel, err := filepath.Rel(root, f.Name())
+		if err != nil {
+			err = walkFun(f.Name(), nil, fmt.Errorf("cannot process file %s", f.Name()))
 			if err != nil {
 				return err
 			}
-			if bytes.Contains(b, []byte("Websocket()")) {
-				opts.warnings = append(opts.warnings,
-					fmt.Sprintf("buffalo.Context#Websocket has been deprecated in v0.11.0, and removed in v0.12.0. Use github.com/gorilla/websocket directly. [%s]", path),
-				)
-			}
-			if bytes.Contains(b, []byte("meta.Name")) {
-				opts.warnings = append(opts.warnings,
-					fmt.Sprintf("meta.Name has been deprecated in v0.11.0, and removed in v0.12.0. Use github.com/markbates/inflect.Name directly. [%s]", path),
-				)
-			}
-			if bytes.Contains(b, []byte("generators.Find(")) {
-				opts.warnings = append(opts.warnings,
-					fmt.Sprintf("generators.Find(string) has been deprecated in v0.11.0, and removed in v0.12.0. Use generators.FindByBox() instead. [%s]", path),
-				)
-			}
-			// i18n middleware changes in v0.11.1
-			if bytes.Contains(b, []byte("T.CookieName")) {
-				b = bytes.Replace(b, []byte("T.CookieName"), []byte("T.LanguageExtractorOptions[\"CookieName\"]"), -1)
-			}
-			if bytes.Contains(b, []byte("T.SessionName")) {
-				b = bytes.Replace(b, []byte("T.SessionName"), []byte("T.LanguageExtractorOptions[\"SessionName\"]"), -1)
-			}
-			if bytes.Contains(b, []byte("T.LanguageFinder=")) || bytes.Contains(b, []byte("T.LanguageFinder ")) {
-				opts.warnings = append(opts.warnings,
-					fmt.Sprintf("i18n.Translator#LanguageFinder has been deprecated in v0.11.1, and has been removed in v0.12.0. Use i18n.Translator#LanguageExtractors instead. [%s]", path),
-				)
-			}
-			return os.WriteFile(path, b, 0o664)
-		})
+		}
 
-		return err
+		if strings.HasPrefix(rel, "..") {
+			continue
+		}
+
+		file, ok := f.(fs.File)
+		if !ok {
+			return fmt.Errorf("cannot process file %s", f.Name())
+		}
+
+		info, err := file.Stat()
+
+		err = walkFun(f.Name(), info, err)
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
