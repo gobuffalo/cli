d350af64ac23138c6f3d99aadffc2fb0634dcc68
Antonio Pagano <apagano@wawand.co>
2022-06-29 09:40:18 -0500

    task: merging

--- internal/tools/pop/command.go
+++ internal/tools/pop/command.go
@@ -1,34 +1,5 @@
 package pop
 
-<<<<<<< HEAD
-import "context"
-
-var Command = &command{}
-
-type command struct{}
-
-func (c command) Name() string {
-	return "pop"
-}
-
-func (c command) Aliases() []string {
-	return []string{"db"}
-}
-
-func (c command) Usage() string {
-	return "buffalo pop [subcommand] [flags] [...]"
-}
-
-func (c command) HelpText() string {
-	return "Manage your database with Pop"
-}
-
-// func (c command) LongHelpText() string {
-// 	return "Manage your database with Pop [TODO: Subcommands]"
-// }
-
-func (c command) Main(ctx context.Context, pwd string, args []string) error {
-=======
 import (
 	"context"
 
@@ -62,6 +33,5 @@ func (c *command) Receive(pls plugin.Plugins) {
 }
 
 func (c *command) Main(ctx context.Context, pwd string, args []string) error {
->>>>>>> bdd723c64171956e9ae1272aaddbc7c1d3f9bdda
 	return nil
 }
