e700406913233a7074a3803adeee1fe98dd585c4
Antonio Pagano <apagano@wawand.co>
2022-06-30 11:12:36 -0500

    task: adding comment on the pop package

---
+++ internal/tools/pop/pop.go
@@ -0,0 +1,6 @@
+// pop package contains the plugins of the pop library
+// these should live in the pop repo and not in the
+// cli repo. These plugins could be default but the user
+// can opt out from using them by compiling their cli
+// with different plugins.
+package pop
