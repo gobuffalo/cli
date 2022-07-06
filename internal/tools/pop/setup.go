46e8b25522ce42b0744dc7d7325279acb8f3d4e0
Antonio Pagano <apagano@wawand.co>
2022-07-06 15:55:09 -0500

    task: moving setup to use the pop libray

--- internal/tools/pop/setup.go
+++ internal/tools/pop/setup.go
@@ -4,11 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"os"
-	"os/exec"
-	"strings"
 
 	"github.com/gobuffalo/meta"
+	"github.com/gobuffalo/pop/v6"
 )
 
 var Setup = &setup{
@@ -45,40 +43,41 @@ func (c *setup) ParseFlags(args []string) (*flag.FlagSet, error) {
 	return c.flagSet, nil
 }
 
-// TODO: Move these exec to use the package
 func (c *setup) Setup(app meta.App) error {
 	if !app.WithPop {
 		return nil
 	}
 
 	if c.dropDatabase {
-		err := run(exec.Command("buffalo", "pop", "drop", "-a"))
-		if err != nil {
-			return fmt.Errorf("We encountered an error when trying to drop your application's databases. Please check to make sure that your database server is running and that the username and passwords found in the database.yml are properly configured and set up on your database server.\n %s", err)
+		for _, cx := range pop.Connections {
+			err := pop.DropDB(cx)
+			if err == nil {
+				return fmt.Errorf("We encountered an error when trying to drop your application's databases. Please check to make sure that your database server is running and that the username and passwords found in the database.yml are properly configured and set up on your database server.\n %s", err)
+			}
 		}
 	}
 
-	err := run(exec.Command("buffalo", "pop", "create", "-a"))
-	if err != nil {
-		return fmt.Errorf("We encountered an error when trying to create your application's databases. Please check to make sure that your database server is running and that the username and passwords found in the database.yml are properly configured and set up on your database server.\n %s", err)
+	for _, cx := range pop.Connections {
+		err := pop.CreateDB(cx)
+		if err == nil {
+			return fmt.Errorf("We encountered an error when trying to create your application's databases. Please check to make sure that your database server is running and that the username and passwords found in the database.yml are properly configured and set up on your database server.\n %s", err)
+		}
 	}
 
-	// Running migrations on the database with Pop
-	err = run(exec.Command("buffalo", "pop", "migrate"))
+	conn := pop.Connections["development"]
+	if conn == nil {
+		return fmt.Errorf("no connection named development")
+	}
+
+	mig, err := pop.NewFileMigrator("migrations", conn)
 	if err != nil {
-		return fmt.Errorf("We encountered the following error when trying to migrate your database:\n%s", err)
+		return err
+	}
+
+	_, err = mig.UpTo(0)
+	if err != nil {
+		return err
 	}
 
 	return nil
-
-}
-
-func run(cmd *exec.Cmd) error {
-	fmt.Printf("--> %s\n", strings.Join(cmd.Args, " "))
-
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-
-	return cmd.Run()
 }
