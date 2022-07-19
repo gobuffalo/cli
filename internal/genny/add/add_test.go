cbd373a1863cf0dfdb0d680d7602a4e56f43505c
Victor Bersy <victor.bersy@hey.com>
2022-07-19 20:30:39 +0200

    Revert "Fix tests"
    This reverts commit 0f3b7780bfd0eb6b828361d4b6fda726a1ddc61c.

--- internal/genny/add/add_test.go
+++ internal/genny/add/add_test.go
@@ -41,7 +41,7 @@ func Test_New(t *testing.T) {
 		r.Equal(ecmds[i], strings.Join(c.Args, " "))
 	}
 
-	efiles := []string{"config/buffalo-plugins.toml", "bin/buffalo-pop"}
+	efiles := []string{"bin/buffalo-pop", "config/buffalo-plugins.toml"}
 	r.Len(res.Files, len(efiles))
 	for i, f := range res.Files {
 		r.True(strings.HasSuffix(f.Name(), efiles[i]))
